cb5dcebfd283b9cf18f3ef65544f31f4850c45d1
vatsav13 <33395446+vatsav13@users.noreply.github.com>
2017-12-02 01:10:16 +0530

    Add files via upload

--- final.s
+++ final.s
@@ -4,14 +4,21 @@
      EXPORT __main
 	 ENTRY 
 __main  FUNCTION		 
-    MOV r1,#28
-	MOV r2,#21
-loop	CMP r1,r2; comparing two numbers in this case 28,21
-	SUBGT r1,r1,r2; subtract r1 from r2 if greater than
-	MOV r3,r1
-	SUBLT r2,r2,r1; subtract r2 from r1 if less than
-	MOV r3,r1; result is stored in r3
-	BNE loop; unless branch is not equal to loop always
+    MOV r1,#14
+	MOV r2,#15
+	MOV r3,#12
+loop0	CMP r1,r2; comparing the initial two numbers considered 14,15
+	BCC loop1; if carry is produced on subtraction go to loop1
+	CMP r1,r3
+	BCC loop3; comparing values 14, 12, if carry is produced on subtraction then go to loop3
+	MOV r4,r1
+	BGT loop0; if branch is greater then loop always
+loop1 CMP r2,r3
+	BCC loop3; on comparing 15,12 if carry is produced on subtraction go to loop3
+	MOV r4,r2
+	BGT loop1
+loop3 MOV r4,r3; on running the program, greatest number is present in r4
+	BGT loop3
 stop B stop ; stop program
      ENDFUNC
      END
