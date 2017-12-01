762dce914f6bfb706ba63b21261c6c3af5bd08e9
vatsav13 <33395446+vatsav13@users.noreply.github.com>
2017-12-02 01:04:11 +0530

    Add files via upload

--- final.s
+++ final.s
@@ -4,14 +4,18 @@
      EXPORT __main
 	 ENTRY 
 __main  FUNCTION		 
-    MOV r1,#14
-	MOV r2,#2
-	udiv r3,r1,r2; performing division operation
-	mls r4,r3,r2,r1; computing modulus of the operation
-	cmp r4,#0; checking if the result of modulus is 0 or 1
-	ITE GT
-	MOVGT r4,#13
-	MOVLE r4,#14
+        MOV r1,#0
+		MOV r2,#1
+		MOV r4,#10; no of terms to be in the series
+		MOV r5,#0x20000000; starting address of the series
+loop	CMP r4,#1
+		ADDS r3,r1,r2; adding the values in the two registers r1,r2 and storingin r3
+		MOV r1,r2
+		MOV r2,r3
+		SUBGT r4,r4,#1; subtract if greater than
+		STR r3,[r5]
+        ADD r5,r5,#4
+		BGT loop; if branch is greater then loop always
 stop B stop ; stop program
      ENDFUNC
      END
